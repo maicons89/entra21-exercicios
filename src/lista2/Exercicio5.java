069a0ffbc299fd2f73d57947bcdeb36fb25bec71
Maicon Santos <maicon.santos89@hotmail.com>
2022-05-08 12:35:39 -0300

    Adicionando Exercicio5

---
+++ src/lista2/Exercicio5.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package lista2;
+
+/**
+ * @author Paraibah89
+ *
+ */
+public class Exercicio5 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
